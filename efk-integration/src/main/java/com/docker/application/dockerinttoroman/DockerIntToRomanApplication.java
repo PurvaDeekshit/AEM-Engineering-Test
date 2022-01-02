6de94b421e2d56d51dc84abad387163abfbef483
PurvaDeekshit <47338443+PurvaDeekshit@users.noreply.github.com>
2022-01-01 19:11:49 -0800

    Add files via upload

---
+++ efk-integration/src/main/java/com/docker/application/dockerinttoroman/DockerIntToRomanApplication.java
@@ -0,0 +1,13 @@
+package com.docker.application.dockerinttoroman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntToRomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerIntToRomanApplication.class, args);
+	}
+
+}
