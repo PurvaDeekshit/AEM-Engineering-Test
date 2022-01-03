a159b24ac01586de52675f95ab636370c48c911b
PurvaDeekshit <47338443+PurvaDeekshit@users.noreply.github.com>
2022-01-02 21:33:11 -0800

    Add files via upload

---
+++ jaeger-integration/src/test/java/com/docker/application/dockerinttoroman/DockerIntToRomanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.application.dockerinttoroman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntToRomanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
